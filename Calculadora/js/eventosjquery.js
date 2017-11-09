1a6bf3f0e1951736fdd7d4a8200bddaf94f1fe8d
JesusAlbertoPadilla <padilladx@hotmail.com>
2017-11-09 15:59:37 -0700

    Curriculum

--- Calculadora/js/eventosjquery.js
+++ Calculadora/js/eventosjquery.js
@@ -1,16 +1,30 @@
 // jquery == $
 
 var inicia=function(){
+	var nuevo=function(){
+		$.ajax({
+  			url: 'https://randomuser.me/api/',
+  			dataType: 'json',
+  			success: function(data) {
+  		$("#nombre").html(data.results[0].name.title+" "+data.results[0].name.first+" "+data.results[0].name.last);
+  		$("#localizacion").html("Street:"+data.results[0].location.street+"<br>"
+  			+"City:"+data.results[0].location.city+"<br>"
+  			+"State:"+data.results[0].location.state+"<br>"
+  			+"Postcode:"+data.results[0].location.postcode);
+  		$("#email").html("Email:"+data.results[0].email);
+  		$("#login").html("Login:"data.results[0].login);
+
+
+  	//alert(data.results[0].name.first+" "+data.results[0].name.last)
+    //console.log(data);
+  			}
+		});
+	}
 	//alert("Lista la pagina");
 	//JSON= JavaScript Object Notation
-	$.ajax({
-  url: 'https://randomuser.me/api/',
-  dataType: 'json',
-  success: function(data) {
-  	alert(data.results[0].name.first+" "+data.results[0].name.last)
-    //console.log(data);
-  }
-});
+	
+
+		$("#btnNuevo").on("click",nuevo);
 }
 //Iniciamos JQuery
 //Cuando la pagina esta lista
