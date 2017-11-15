24032a68884124ad5a05197416561d1a2425653f
JesusAlbertoPadilla <padilladx@hotmail.com>
2017-11-15 16:01:19 -0700

    Ejecutable

---
+++ Calculadora2/js/calculadora.js
@@ -0,0 +1,40 @@
+//Variable global
+var operador="";
+function numero(num){
+	if (operador=="") { //Operando1
+		var valorinicial=document.calculadora.Operando1.value;
+		if (valorinicial=="0") {
+			document.calculadora.Operando1.value="";
+		}
+		//concatena los valores de num
+		//con los del operando1
+		document.calculadora.Operando1.value=
+		document.calculadora.Operando1.value + num;
+
+	}
+	else{ //Operando2
+		var valorinicial=document.calculadora.Operando2.value;
+		if (valorinicial=="0") {
+			document.calculadora.Operando2.value="";
+		}
+		document.calculadora.Operando2.value=
+		document.calculadora.Operando2.value + num;
+	}
+}
+
+function operadores(ope){
+	operador=ope
+}
+
+function igual(){
+	var valor1=document.calculadora.Operando1.value;
+	var valor2=document.calculadora.Operando2.value;
+	document.calculadora.Resultado.value= eval(valor1+operador+valor2);//eval=toma string que sea una operacion matematica y lo resuelve
+}
+
+function borrar(){
+	operador="";
+	document.calculadora.Operando1.value=0;
+	document.calculadora.Operando2.value=0;
+	document.calculadora.Resultado.value=0;
+}
